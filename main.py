cce81e1f2055f6711735d271176bfa2724107df9
Django Heimgartner <django.heimgartner@gmail.com>
2022-09-09 22:35:20 +0200

    Add more logic to main, to easy change settings

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
 import sys
+from pygame.locals import *
 from settings import *
 from level import Level
 
 
 # Pygame setup
 pygame.init()
+pygame.display.set_caption("Adventure")
 screen = pygame.display.set_mode((screen_width, screen_height))
-clock = pygame.time.Clock()
+
+# Video & display
+fps = 60
+fps_clock = pygame.time.Clock()
+
 level = Level(level_map, screen)
 
+"""
+The game loop that runs
+the actual game
+"""
 while True:
+
     for event in pygame.event.get():
+        # Exit when the close window button is clicked
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
@@ -20,4 +32,4 @@ while True:
     level.run()
 
     pygame.display.update()
-    clock.tick(60)
+    fps_clock.tick(fps)
